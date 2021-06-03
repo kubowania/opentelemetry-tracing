3ec7e0d36842526f881c9f67479177151b45d8d5
Ania Kubow <51362610+kubowania@users.noreply.github.com>
2021-06-03 16:01:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || "8080";
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.json("Hello World");
+});
+
 app.get("/date", (req, res) => {
   res.json({ today: new Date() });
 });
